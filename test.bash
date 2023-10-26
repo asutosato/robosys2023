a064e50d54c5a442c1f35bc5817e520543cdda40
Asuto Sato <ast1639st@gmail.com>
2023-10-26 13:21:40 +0900

    add STRANGE INPUT

--- test.bash
+++ test.bash
@@ -1,6 +1,7 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2023 Asuto Sato ast1639st@gmail.com
 # SPDX-License-Identifier: BSD-3-Clause
+
 
 ng () {
         echo NG at Line ${1}
@@ -9,9 +10,19 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus)
+  ### I/O ###
+  out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ] || ng ${LINENO}
+  [ "${out}" = 15 ] || ng ${LINENO}
+
+  ### STRANGE INPUT ###
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
+  out=$(echo | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
