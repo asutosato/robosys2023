0f052233bad291f197bc2e19304a6961eb5fb997
Asuto Sato <ast1639st@gmail.com>
2023-10-26 12:50:40 +0900

    add test

--- test.bash
+++ test.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+ng () {
+        echo NG at Line ${1}
+       res=1
+}
+
+res=0
+
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 14 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
